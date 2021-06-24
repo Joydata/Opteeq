74beecde6425e2d7283eca8a827b69458a4ba5ca
johann yates <johannyates@gmail.com>
2021-06-24 10:28:30 +0200

    Function added to format file for VIA import

--- Google_vision.py
+++ Google_vision.py
@@ -9,7 +9,7 @@ from google.cloud import vision
 # Environment variable must be set before the function can be used
 # Terminal command : set GOOGLE_APPLICATION_CREDENTIALS=<KEY_FILE_PATH>
 
-def generate_annotations(input_file,output_file):
+def generate_annotations(input_file, output_file):
     """Procedure to generate a csv file with the output of googlevision API text detection
 
     Parameters
@@ -24,10 +24,10 @@ def generate_annotations(input_file,output_file):
     index : index of the box
     text : string
         the text of the given box
-    box_center_x : float
-        the x coordinate of the center of the given box
-    box_center_y : float
-        the y coordinate of the center of the given box
+    box_x : int
+        the x coordinate of the top left corner of the given box
+    box_y : int
+        the y coordinate of the top left corner of the given box
     box_width : int
         the width of the given box
     box_height : int
@@ -45,7 +45,7 @@ def generate_annotations(input_file,output_file):
     response = client.text_detection(image=image)
     
     # Initialize a Dictionnary to store the results
-    result = {'text':[],'box_center_x':[],'box_center_y':[],'box_width':[],'box_height':[]}
+    result = {'text':[],'box_x':[],'box_y':[],'box_width':[],'box_height':[]}
     
     #loop over the boxes
     for box in response.text_annotations:
@@ -73,13 +73,13 @@ def generate_annotations(input_file,output_file):
         # Calculate the center, width and height of the box
         box_width = max_x - min_x
         box_height = max_y - min_y
-        box_center_x = min_x + (box_width / 2)
-        box_center_y = min_y + (box_height / 2)
+        box_x = min_x
+        box_y = min_y
 
         # Store the results in the Dictionnary
         result['text'].append(text_content)
-        result['box_center_x'].append(box_center_x)
-        result['box_center_y'].append(box_center_y)
+        result['box_x'].append(box_x)
+        result['box_y'].append(box_y)
         result['box_width'].append(box_width)
         result['box_height'].append(box_height)
 
@@ -87,6 +87,65 @@ def generate_annotations(input_file,output_file):
     result_df = pd.DataFrame(result)
     result_df.to_csv(output_file)
 
-# To Test of the function, uncomment the following lines
-#file_name = os.path.abspath('1193-receipt.jpg')
-#generate_annotations(file_name,'output.csv')
+def convert_annotations(input_image, input_annotations, output_file):
+    """Procedure to convert an annotation file for further import into VIA
+
+    Parameters
+    ----------
+    input_image : string
+        Path of the image file
+    input_annotations : string
+        Path of the csv file with annotations generated with generate_annotations function
+    output_file : string
+        Path of the output csv file
+
+    Output file
+    -------
+    csv file with the right format to import in VIA
+    """
+    # Load the annotations in a DataFrame
+    annotations = pd.read_csv(input_annotations)
+
+    # Initialize a Dictionnary to store the results
+    result={'filename':[],'file_size':[],'file_attributes':[],'region_count':[],'region_id':[],'region_shape_attributes':[],'region_attributes':[]}
+    
+    # Prepare the file information    
+    file_size = os.path.getsize(input_image)
+    file_attributes = '{"caption":"","public_domain":"no","image_url":""}'
+    
+    # Prepare the count of annotations 
+    region_count = len(annotations)
+
+    # Loop over annotations
+    for idx in annotations.index:
+        # Get coordinates of the box
+        X = int(annotations.iloc[idx]['box_x'])
+        Y = int(annotations.iloc[idx]['box_y'])
+        WIDTH = annotations.iloc[idx]['box_width']
+        HEIGHT = annotations.iloc[idx]['box_height']
+        
+        # Get the text inside the box. New-line characters are replaced by space characters
+        TEXT = annotations.iloc[idx]['text'].replace('\n',' ')
+        
+        # Prepare the region_shape attributes and region attributes 
+        region_shape_attributes = f'{{"name":"rect","x":{X},"y":{Y},"width":{WIDTH},"height":{HEIGHT}}}'
+        region_attributes = f'{{"name":"{TEXT}","type":"unknown","image_quality":{{"good":true,"frontal":true,"good_illumination":true}}}}'
+        
+        # Store all information in the dictionnary
+        result['filename'].append(input_image)
+        result['file_size'].append(file_size)
+        result['file_attributes'].append(file_attributes)
+        result['region_count'].append(region_count)
+        result['region_id'].append(idx)
+        result['region_shape_attributes'].append(region_shape_attributes)
+        result['region_attributes'].append(region_attributes)
+    
+    # Store the results in the output file
+    result_df = pd.DataFrame(result)
+    result_df.to_csv(output_file, header=True, index=False)
+
+
+# To Test of the functions, uncomment the following lines
+#file_name = '1193-receipt.jpg'
+#generate_annotations(file_name, 'annotations.csv')
+#convert_annotations('1193-receipt.jpg', 'annotations.csv', 'via_csv.csv')
